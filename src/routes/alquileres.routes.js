406d0660f6d1aef5622b99962a4628983e59e353
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-09 09:06:51 -0500

    feat: :sparkles: Quinto EndPoint

--- src/routes/alquileres.routes.js
+++ src/routes/alquileres.routes.js
@@ -1,5 +1,6 @@
 import { con } from '../database/connection.js'
 import { Router } from 'express'
+import { ObjectId } from 'mongodb'
 
 const appAlquileres = Router()
 const db = await con()
@@ -40,6 +41,93 @@ appAlquileres.get('/alquileresActivos', async (req, res) => {
         dni: { $first: '$dni' },
         telefono: { $first: '$telefono' },
         alquileres: { $push: '$alquileres' }
+      }
+    }
+  ]).toArray()
+  res.send(result)
+})
+
+// 5. Obtener los detalles del alquiler con el ID_Alquiler específico.
+appAlquileres.get('/detallesAlquiler/:_id', async (req, res) => {
+  const { _id } = req.params
+  const idAlquiler = _id
+  const alquiler = db.collection('alquiler')
+  const result = await alquiler.aggregate([
+    {
+      $match: {
+        _id: new ObjectId(idAlquiler)
+      }
+    },
+    {
+      $lookup: {
+        from: 'automovil',
+        localField: 'id_automovil',
+        foreignField: '_id',
+        as: 'automovil'
+      }
+    },
+    {
+      $lookup: {
+        from: 'cliente',
+        localField: 'id_cliente',
+        foreignField: '_id',
+        as: 'cliente'
+      }
+    },
+    {
+      $unwind: '$automovil'
+    },
+    {
+      $unwind: '$cliente'
+    },
+    {
+      $project: {
+        _id: 0,
+        id_alquiler: { $toString: '$_id' },
+        estado: 1,
+        fechas: {
+          fecha_inicio: {
+            $dateToString: {
+              format: '%Y-%m-%d',
+              date: '$fecha_inicio'
+            }
+          },
+          fecha_fin: {
+            $dateToString: {
+              format: '%Y-%m-%d',
+              date: '$fecha_fin'
+            }
+          }
+        },
+        id_automovil: { $toString: '$automovil._id' },
+        marca: '$automovil.marca',
+        modelo: '$automovil.modelo',
+        cliente_nombre: { $concat: ['$cliente.nombre', ' ', '$cliente.apellido'] },
+        id_cliente: { $toString: '$cliente._id' }
+      }
+    },
+    {
+      $group: {
+        _id: '$id_cliente',
+        alquileres: {
+          $push: {
+            id_alquiler: '$id_alquiler',
+            id_automovil: '$id_automovil',
+            fechas: '$fechas',
+            marca: '$marca',
+            modelo: '$modelo',
+            estado: '$estado'
+          }
+        },
+        cliente_nombre: { $first: '$cliente_nombre' }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        id_cliente: '$_id',
+        cliente_nombre: 1,
+        alquileres: 1
       }
     }
   ]).toArray()
