5c5fb862fa2b762644faaa70e3dbcede14dd09c7
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-10 08:27:57 -0500

    feat: :sparkles: Catorceavo EndPoint

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -20,4 +20,58 @@ appCliente.get('/cliente/:dni', async (req, res) => {
   res.send(result)
 })
 
+// 14.Obtener los datos de los clientes que realizaron al menos un alquiler.
+
+appCliente.get('/clientesAlMenosUnAlquiler', async (req, res) => {
+  const alquiler = db.collection('alquiler')
+  const result = await alquiler.aggregate([
+    {
+      $lookup: {
+        from: 'cliente',
+        localField: 'id_cliente',
+        foreignField: '_id',
+        as: 'cliente'
+      }
+    },
+    {
+      $unwind: '$cliente'
+    },
+    {
+      $project: {
+        _id: 0,
+        cliente_nombre: { $concat: ['$cliente.nombre', ' ', '$cliente.apellido'] },
+        id_cliente: { $toString: '$cliente._id' },
+        email: '$cliente.email',
+        dni: '$cliente.dni',
+        telefone: '$cliente.telefono',
+        direccion: '$cliente.direccion'
+      }
+    },
+    {
+      $group: {
+        _id: '$id_cliente',
+        cliente_nombre: { $first: '$cliente_nombre' },
+        datos: {
+          $push: {
+            email: '$email',
+            dni: '$dni',
+            telefone: '$telefone',
+            direccion: '$direccion'
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        id_cliente: '$_id',
+        cliente_nombre: 1,
+        datos: 1
+      }
+    }
+  ]).toArray()
+
+  res.send(result)
+})
+
 export default appCliente
