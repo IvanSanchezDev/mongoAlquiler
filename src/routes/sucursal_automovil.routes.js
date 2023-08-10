5456f320f6f3b3052acce1d26c96dd55ecbad5ec
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-10 08:36:00 -0500

    feat: :sparkles: EndPoint Dieciseisavo

---
+++ src/routes/sucursal_automovil.routes.js
@@ -0,0 +1,59 @@
+import { con } from '../database/connection.js'
+import { Router } from 'express'
+
+const appSucursalAutomovil = Router()
+const db = await con()
+
+// 16. Mostrar la cantidad total de automóviles en cada sucursal junto con su dirección
+
+appSucursalAutomovil.get('/totalAutomoviles', async (req, res) => {
+  try {
+    const sucursalAutomovil = db.collection('sucursal_automovil')
+    const result = sucursalAutomovil.aggregate([
+      {
+        $lookup: {
+          from: 'sucursal',
+          localField: 'id_sucursal',
+          foreignField: '_id',
+          as: 'sucursal'
+        }
+      },
+      {
+        $unwind: '$sucursal'
+      },
+      {
+        $project: {
+          _id: 0,
+          id_sucursal: '_id',
+          direccion: '$sucursal.direccion',
+          cantidad_disponible: '$cantidad_disponible'
+        }
+      },
+      {
+        $group: {
+          _id: '$direccion',
+          sucursal: {
+            $push: {
+              cantidad_disponible: '$cantidad_disponible'
+            }
+          },
+          direccion: { $first: '$direccion' }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          cantidad_total: {
+            $sum: '$sucursal.cantidad_disponible'
+          },
+          direccion: 1
+        }
+      }
+    ]).toArray()
+    res.send(result)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+export default appSucursalAutomovil
