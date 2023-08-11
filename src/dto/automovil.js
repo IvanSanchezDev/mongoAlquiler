9adbcdf6b7994e3e66b4efbb357b75cc9c7c758d
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-11 08:04:37 -0500

    refactor: :recycle: codigo mas limpio

--- src/dto/automovil.js
+++ src/dto/automovil.js
@@ -1,52 +1,52 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-import { Expose } from 'class-transformer';
-import { IsDefined } from 'class-validator';
+import { Expose } from 'class-transformer'
+import { IsDefined } from 'class-validator'
+const __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+  const c = arguments.length; let r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc; let d
+  if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function') r = Reflect.decorate(decorators, target, key, desc)
+  else for (let i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r
+  return c > 3 && r && Object.defineProperty(target, key, r), r
+}
+const __metadata = (this && this.__metadata) || function (k, v) {
+  if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function') return Reflect.metadata(k, v)
+}
 export class Automovil {
-    constructor(data) {
-        Object.assign(this, data);
-        this.marca = "";
-        this.modelo = 0;
-        this.anio = 0;
-        this.tipo = "";
-        this.capacidad = 0;
-        this.precio_diario = 0;
-    }
+  constructor (data) {
+    Object.assign(this, data)
+    this.marca = ''
+    this.modelo = 0
+    this.anio = 0
+    this.tipo = ''
+    this.capacidad = 0
+    this.precio_diario = 0
+  }
 }
 __decorate([
-    Expose({ name: 'marca_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `la marca del automovil es obligatoria` }; } }),
-    __metadata("design:type", String)
-], Automovil.prototype, "marca", void 0);
+  Expose({ name: 'marca_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'la marca del automovil es obligatoria' } } }),
+  __metadata('design:type', String)
+], Automovil.prototype, 'marca', void 0)
 __decorate([
-    Expose({ name: 'modelo_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `el modelo del automovil  es obligatoria` }; } }),
-    __metadata("design:type", Number)
-], Automovil.prototype, "modelo", void 0);
+  Expose({ name: 'modelo_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'el modelo del automovil  es obligatoria' } } }),
+  __metadata('design:type', Number)
+], Automovil.prototype, 'modelo', void 0)
 __decorate([
-    Expose({ name: 'anio_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `el año del  automovil  es obligatoria` }; } }),
-    __metadata("design:type", Number)
-], Automovil.prototype, "anio", void 0);
+  Expose({ name: 'anio_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'el año del  automovil  es obligatoria' } } }),
+  __metadata('design:type', Number)
+], Automovil.prototype, 'anio', void 0)
 __decorate([
-    Expose({ name: 'tipo_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `tl tipo del automovil  es obligatoria` }; } }),
-    __metadata("design:type", String)
-], Automovil.prototype, "tipo", void 0);
+  Expose({ name: 'tipo_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'tl tipo del automovil  es obligatoria' } } }),
+  __metadata('design:type', String)
+], Automovil.prototype, 'tipo', void 0)
 __decorate([
-    Expose({ name: 'capacidad_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `la capacidad del automovil es obligatoria` }; } }),
-    __metadata("design:type", Number)
-], Automovil.prototype, "capacidad", void 0);
+  Expose({ name: 'capacidad_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'la capacidad del automovil es obligatoria' } } }),
+  __metadata('design:type', Number)
+], Automovil.prototype, 'capacidad', void 0)
 __decorate([
-    Expose({ name: 'preciodiario_automovil' }),
-    IsDefined({ message: () => { throw { status: 422, message: `el precio diario del automovil  es obligatoria` }; } }),
-    __metadata("design:type", Number)
-], Automovil.prototype, "precio_diario", void 0);
+  Expose({ name: 'preciodiario_automovil' }),
+  IsDefined({ message: () => { throw { status: 422, message: 'el precio diario del automovil  es obligatoria' } } }),
+  __metadata('design:type', Number)
+], Automovil.prototype, 'precio_diario', void 0)
--- src/dto/cliente.js
+++ src/dto/cliente.js
@@ -1,55 +1,55 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-import { Expose } from 'class-transformer';
-import { IsDefined, IsEmail, IsString } from 'class-validator';
+import { Expose } from 'class-transformer'
+import { IsDefined, IsEmail, IsString } from 'class-validator'
+const __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+  const c = arguments.length; let r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc; let d
+  if (typeof Reflect === 'object' && typeof Reflect.decorate === 'function') r = Reflect.decorate(decorators, target, key, desc)
+  else for (let i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r
+  return c > 3 && r && Object.defineProperty(target, key, r), r
+}
+const __metadata = (this && this.__metadata) || function (k, v) {
+  if (typeof Reflect === 'object' && typeof Reflect.metadata === 'function') return Reflect.metadata(k, v)
+}
 class Direccion {
 }
 __decorate([
-    IsDefined({ message: 'La ciudad es obligatoria en la dirección' }),
-    IsString({ message: 'La ciudad debe ser una cadena de texto' }),
-    __metadata("design:type", String)
-], Direccion.prototype, "ciudad", void 0);
+  IsDefined({ message: 'La ciudad es obligatoria en la dirección' }),
+  IsString({ message: 'La ciudad debe ser una cadena de texto' }),
+  __metadata('design:type', String)
+], Direccion.prototype, 'ciudad', void 0)
 export class Cliente {
-    constructor(data) {
-        Object.assign(this, data);
-        this.direccion = { calle: '', ciudad: '' };
-    }
+  constructor (data) {
+    Object.assign(this, data)
+    this.direccion = { calle: '', ciudad: '' }
+  }
 }
 __decorate([
-    Expose({ name: 'nombre_cliente' }),
-    IsDefined({ message: 'El nombre del cliente es obligatorio' }),
-    __metadata("design:type", String)
-], Cliente.prototype, "nombre", void 0);
+  Expose({ name: 'nombre_cliente' }),
+  IsDefined({ message: 'El nombre del cliente es obligatorio' }),
+  __metadata('design:type', String)
+], Cliente.prototype, 'nombre', void 0)
 __decorate([
-    Expose({ name: 'apellido_cliente' }),
-    IsDefined({ message: 'El apellido del cliente es obligatorio' }),
-    __metadata("design:type", String)
-], Cliente.prototype, "apellido", void 0);
+  Expose({ name: 'apellido_cliente' }),
+  IsDefined({ message: 'El apellido del cliente es obligatorio' }),
+  __metadata('design:type', String)
+], Cliente.prototype, 'apellido', void 0)
 __decorate([
-    Expose({ name: 'dni_cliente' }),
-    IsDefined({ message: 'El DNI del cliente es obligatorio' }),
-    __metadata("design:type", Number)
-], Cliente.prototype, "dni", void 0);
+  Expose({ name: 'dni_cliente' }),
+  IsDefined({ message: 'El DNI del cliente es obligatorio' }),
+  __metadata('design:type', Number)
+], Cliente.prototype, 'dni', void 0)
 __decorate([
-    Expose({ name: 'direccion_cliente' }),
-    IsDefined({ message: 'La dirección es obligatoria' }),
-    __metadata("design:type", Direccion)
-], Cliente.prototype, "direccion", void 0);
+  Expose({ name: 'direccion_cliente' }),
+  IsDefined({ message: 'La dirección es obligatoria' }),
+  __metadata('design:type', Direccion)
+], Cliente.prototype, 'direccion', void 0)
 __decorate([
-    Expose({ name: 'telefono_cliente' }),
-    IsDefined({ message: 'El teléfono es obligatorio' }),
-    __metadata("design:type", Number)
-], Cliente.prototype, "telefono", void 0);
+  Expose({ name: 'telefono_cliente' }),
+  IsDefined({ message: 'El teléfono es obligatorio' }),
+  __metadata('design:type', Number)
+], Cliente.prototype, 'telefono', void 0)
 __decorate([
-    Expose({ name: 'email_cliente' }),
-    IsDefined({ message: 'El email es obligatorio' }),
-    IsEmail({}, { message: 'El email debe ser válido' }),
-    __metadata("design:type", String)
-], Cliente.prototype, "email", void 0);
+  Expose({ name: 'email_cliente' }),
+  IsDefined({ message: 'El email es obligatorio' }),
+  IsEmail({}, { message: 'El email debe ser válido' }),
+  __metadata('design:type', String)
+], Cliente.prototype, 'email', void 0)
