4941bdaebe51664b1689876c7115a14ee75547fe
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-08-11 09:19:48 -0500

    feat: :sparkles: implementacion generacion de token y verificacion

--- src/app.js
+++ src/app.js
@@ -5,16 +5,18 @@ import appAutomovil from './routes/automoviles.routes.js'
 import appEmpleados from './routes/empleados.routes.js'
 import appReservas from './routes/reservas.routes.js'
 import appSucursalAutomovil from './routes/sucursal_automovil.routes.js'
+import { appToken, appVerify } from './helpers/jwt.js'
 
 const app = express()
 app.use(express.json())
 
-app.use('/api/cliente', appCliente)
-app.use('/api/alquiler', appAlquileres)
-app.use('/api/automovil', appAutomovil)
-app.use('/api/empleado', appEmpleados)
-app.use('/api/reserva', appReservas)
-app.use('/api/sucursalAutomovil', appSucursalAutomovil)
+app.use('/api/cliente', appVerify, appCliente)
+app.use('/api/alquiler', appVerify, appAlquileres)
+app.use('/api/automovil', appVerify, appAutomovil)
+app.use('/api/empleado', appVerify, appEmpleados)
+app.use('/api/reserva', appVerify, appReservas)
+app.use('/api/sucursalAutomovil', appVerify, appSucursalAutomovil)
+app.use('/token', appToken)
 
 app.use((req, res) => {
   res.send('404 not found')
